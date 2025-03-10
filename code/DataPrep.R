cb3c57e3884cee2652a469df97f3a0d09d1d520a
joRauser <jonas.rauser@web.de>
2025-03-10 23:26:16 +0100

    Include timespan-Alignment and better Data-Wrangling (especially for the wastewater-data) Also some commentaries are added

--- code/DataPrep.R
+++ code/DataPrep.R
@@ -4,42 +4,64 @@ setwd("/Users/jonasrauser/Desktop/R/Comparison-of-different-Rt-Estimates-from-wa
 
 ### Load Data
 
-cohort_raw <- read.csv("cohortData_RLP.csv", header = T, sep = ";")
-wastewater_raw <- read.csv("completeData_RLP.csv", header = T, sep = ";")
+cohort_raw <- read.csv("cohortData_RLP.csv", header = T, sep = ";")%>%
+  rename(Date = Date.of.SARS.CoV.2.test)
+
+wastewater_raw <- read.csv("completeData_RLP.csv", header = T, sep = ";")%>%
+  rename(Date = Datum.der.Entnahme)%>%
+  filter(Probe.Valide == "ja")
+
 hospitalizations_raw <- read.csv("Hospitalisierungen.csv", header = T, sep = ",")%>%
-  filter(Bundesland == "Rheinland-Pfalz")
+  filter(Bundesland == "Rheinland-Pfalz")%>%
+  rename(Date = Datum)
 
 
 
 ### Clean Data
 
-# Remove all irrelevant features
+## Wastewater-data
 wastewater_cleaned <- wastewater_raw %>%
-  select(-"X", -"Kläranlagen.ID", -"Probenbegleitschein.vollständig", -"Niederschläge.am.Vortag.der.Probenentnahme..mm.", 
-         -"Niederschläge.am.Probenentnahmetag..mm.", -"Lufttemperatur.am.Probenentnahmetag..Grad.Celsius.", 
-         -"Chemischer.Sauerstoffbedarf.CSB..mg.l.", -"Total.Organic.Carbon.TOC..mg.l.")
+  # Select relevant features
+  select(Date, Genkopien.im.Durchschnitt..Gq.ml., Genkopien.N1.ml, 
+         Genkopien.N2.ml, Anteil.Genkopien.Durchschnitt.zu.PMMoV.x100.000, Probenvolumen..ml.) %>%
+  # Set all values as numeric
+  mutate(across(
+    c(`Genkopien.im.Durchschnitt..Gq.ml.`, `Genkopien.N1.ml`, `Genkopien.N2.ml`, `Anteil.Genkopien.Durchschnitt.zu.PMMoV.x100.000`),
+    as.numeric)) %>%
+  # summarize per Date
+  group_by(Date) %>%
+  summarise(genCopiesAvg = sum(Genkopien.im.Durchschnitt..Gq.ml., na.rm = TRUE),
+            genCopiesN1 = sum(Genkopien.N1.ml, na.rm = TRUE),
+            genCopiesN2 = sum(Genkopien.N2.ml, na.rm = TRUE),
+            genCopiesToPMMoV = sum(Anteil.Genkopien.Durchschnitt.zu.PMMoV.x100.000, na.rm = TRUE))
 
 
-# Remove Extra Rows from differentation in Age-group ("Altergruppe") and sum values up on the same date 
-# => Set on only one row per Date
+## Hospitalization-data
 hospitalizations_cleaned <- hospitalizations_raw %>%
   select(-Altersgruppe, -Bundesland_Id) %>% 
-  group_by(Datum) %>% 
+  # summarize per Date
+  group_by(Date) %>% 
   summarise(Fallzahlen = sum(X7T_Hospitalisierung_Faelle, na.rm = TRUE))
-hospitalizations_cleaned$Datum <- as.Date(hospitalizations_cleaned$Datum, format = "%Y-%m-%d")
+
+# hospitalizations_cleaned$Datum <- as.Date(hospitalizations_cleaned$Date, format = "%Y-%m-%d")
 
 
-# Similar thing for cohort-Study-data :) 
-cohort_clenaed <- cohort_raw %>%
+## SentiSurv-data
+cohort_cleaned <- cohort_raw %>%
   select(-Town,-Study.phase) %>%
-  group_by(Date.of.SARS.CoV.2.test) %>% 
+  group_by(Date) %>% 
   summarise(Valid.test = sum(Valid.test, na.rm = TRUE), 
             Positive.test = sum(Positive.test, na.rm = TRUE),
             Newly.positive.test = sum(Newly.positive.test, na.rm = TRUE))
 
 
-##### Function to get the same time-span on all datasets
+##### Function to set time-span on all datasets (is the maximal timespan, where data is available on every dataset)
 
-#alignTimespan <- function(df, time_start, time_end){
-  
-#}
+alignTimespan <- function(df){
+  df %>% mutate(Date = as.Date(Date, format = "%Y-%m-%d"))%>%
+    filter(Date >= as.Date("2023-01-01") & Date <= as.Date("2023-10-01"))
+}
+
+alignTimespan(wastewater_cleaned)
+alignTimespan(hospitalizations_cleaned)
+alignTimespan(cohort_cleaned)
